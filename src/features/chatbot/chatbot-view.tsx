b34c7f1b2581c1a930df7513747dd25c4b1a6e24
anpigon <markan82@gmail.com>
2024-05-11 22:16:16 +0900

    feat: Add new icon components
    This commit adds new icon components: IconCopy, IconEdit, IconRegenerate, and IconTrash. These components are used to render SVG icons for copy, edit, regenerate, and trash actions respectively.
    
    Note: Please remove this note before committing.

--- src/features/chatbot/chatbot-view.tsx
+++ src/features/chatbot/chatbot-view.tsx
@@ -3,16 +3,20 @@ import {useSettings} from '@/hooks/useApp';
 import {ChatbotContainer} from './components/chatbot-container';
 import {ChatbotHeader} from './components/chatbot-header';
 import {MessageContainer} from './components/message-container';
+import {UserMessage} from './components/user-message';
 
 export const ChatbotView: React.FC = () => {
 	const settings = useSettings();
 	const chatbotName = settings?.appearance?.chatbotName ?? DEFAULT_SETTINGS.appearance.chatbotName;
 	const modelName = settings?.general.model || DEFAULT_SETTINGS.general.model;
+	const username = settings?.appearance.userName || DEFAULT_SETTINGS.appearance.userName;
 
 	return (
 		<ChatbotContainer>
 			<ChatbotHeader chatbotName={chatbotName} modelName={modelName} />
-			<MessageContainer />
+			<MessageContainer>
+				<UserMessage username={username} message="test" />
+			</MessageContainer>
 		</ChatbotContainer>
 	);
 };
