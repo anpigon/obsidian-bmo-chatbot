bc02c8b365d6c8077b53b9c62020fd9f184d5b56
anpigon <markan82@gmail.com>
2024-05-12 01:23:09 +0900

    refactor: Remove unused code and update headers in request-fetch.ts

--- src/utils/request-fetch.ts
+++ src/utils/request-fetch.ts
@@ -7,30 +7,13 @@ export const requestFetch: ClientOptions['fetch'] = async (url, init) => {
 	Logger.info(url, init);
 	const headers = init?.headers as unknown as Record<string, string>;
 	const body = JSON.stringify(JSON.parse(`${init?.body ?? {}}`));
-	Logger.info('headers', headers);
-	Logger.info('body', body);
 
 	const response = await requestUrl({
 		url: `${url}`,
 		method: 'POST',
-		// headers: init?.headers as any,
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: headers['authorization'],
-		},
+		headers,
 		body,
-		// body: `${init?.body}`,
-		// body: JSON.stringify({
-		// 	model: 'xionic-ko-llama-3-70b',
-		// 	temperature: 1,
-		// 	top_p: 1,
-		// 	frequency_penalty: 0,
-		// 	presence_penalty: 0,
-		// 	n: 1,
-		// 	stream: false,
-		// 	messages: [],
-		// }),
 	});
 	Logger.info('response', response.json);
-	return response as unknown as Promise<Response>;
+	return response.json as unknown as Promise<Response>;
 };
