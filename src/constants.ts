3510e48792c556ace7fc3018456732d5508435fc
anpigon <markan82@gmail.com>
2024-05-12 09:52:51 +0900

    feat: Add constants for XIONIC integration

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const XIONIC_MODEL = 'xionic-ko-llama-3-70b';
+export const XIONIC_API_KEY = '934c4bbc-c384-4bea-af82-1450d7f8128d';
+export const XIONIC_API_URL = 'https://api.groq.com/openai/v1';
